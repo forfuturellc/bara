6a23e4f73175782d8771a330d13df8ee073cfa18
Gocho Mugo <mugo@forfuture.co.ke>
2015-01-23 14:49:47 +0300

    Posting notification demo-implementation
    The endpoint, "/notifications", will be used to process POST
    requests with data for new notifications.

---
+++ src/main/java/com/forfuture/data/NotificationPayload.java
@@ -0,0 +1,12 @@
+package com.forfuture.data;
+
+/**
+ * Created by gocho on 1/23/15.
+ */
+public class NotificationPayload {
+
+    public String userId;
+    public String username;
+    public String message;
+
+}
--- src/main/java/com/forfuture/router/NotificationController.java
+++ src/main/java/com/forfuture/router/NotificationController.java
@@ -2,11 +2,13 @@ package com.forfuture.router;
 
 import com.forfuture.data.Country;
 import com.forfuture.data.Notification;
+import com.forfuture.data.NotificationPayload;
 import com.forfuture.data.Route;
 import com.forfuture.data.transport.RoadTransport;
 import com.forfuture.data.user.User;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,19 +24,32 @@ import java.util.HashMap;
 @RestController
 public class NotificationController {
     // Sample notifications
-    Country kenya = new Country(1, "kenya");
-    Route route1 = new Route(1, "125", "nairobi - cbd", "ongata rongai", kenya, RoadTransport.getInstance());
-    User user1 = new User(1, "gocho", "mugo@forfuture.co.ke");
-    Notification notification1 = new Notification(1, Notification.NotificationType.TrafficJam, route1,user1 , "accident at strathmore");
-    Notification notification2 = new Notification(2, Notification.NotificationType.TrafficJam, route1,user1 , "traffic jam globe roundabout");
+    static int numNotifications = 0;
+    static Country kenya = new Country(1, "kenya");
+    static Route route1 = new Route(1, "125", "nairobi - cbd", "ongata rongai", kenya, RoadTransport.getInstance());
+    static User user1 = new User(1, "gocho", "mugo@forfuture.co.ke");
+    static Notification notification1 = new Notification(++numNotifications, Notification.NotificationType.TrafficJam, route1,user1 , "accident at strathmore");
+    static Notification notification2 = new Notification(++numNotifications, Notification.NotificationType.TrafficJam, route1,user1 , "traffic jam globe roundabout");
+
+    private static ArrayList<Notification> notifications = new ArrayList<Notification>(){{
+        add(notification1); add(notification2);
+    }};
 
     @RequestMapping(value = "/notifications", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String, ArrayList<Notification>> getAllNotifications() {
         HashMap<String, ArrayList<Notification>> response = new HashMap<String, ArrayList<Notification>>();
-        ArrayList<Notification> notifications = new ArrayList<Notification>();
-        notifications.add(notification1);
-        notifications.add(notification2);
         response.put("notifications", notifications);
         return response;
     }
+
+    @RequestMapping(value = "/notifications", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public HashMap<String, String> postNotification(@RequestParam("userId") String userId,
+                                                    @RequestParam("username") String username, @RequestParam("message") String message) {
+        Notification newNotification = new Notification(++numNotifications, Notification.NotificationType.TrafficJam, route1, user1, message);
+        notifications.add(newNotification);
+        HashMap<String, String> response = new HashMap<String, String>();
+        response.put("message", "succeeded!");
+        return response;
+    }
+
 }
